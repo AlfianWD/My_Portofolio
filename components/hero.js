10c9a1169dd6e6f4f199373f0da6a63dbc1ada6f
AlfianWD <carzyman01@gmail.com>
2026-05-20 15:57:17 +0700

    refactor(hero) : optimize responsive design structure

--- components/hero.js
+++ components/hero.js
@@ -3,12 +3,12 @@ export default {
         <section class="hero mt-8 grid grid-cols-2 gap-8 px-16 items-center justify-items-center">
 
             <!-- Hero Left Section -->
-                <div class="hero-left justify-items-center">
+                <div class="hidden hero-left justify-items-center md:flex">
                     <img src="src/assets/img/My_IMG-01.png" alt="My_IMG-01">
                 </div>
 
             <!-- Hero Right Section -->
-                <div class="hero-right content-center font-first ltr">
+                <div class="hero-right content-center font-first ltr ml-8">
                     <p class="text-[#FCE05A] text-[24px] my-8">INTRODUCTION</p>
                     <h1 class="text-white text-[40px] mb-8">WEB DEVELOPER</h1>
                     <a href="#contact" class="rounded-none bg-[#FCE05A] p-2">Contact Me</a>
